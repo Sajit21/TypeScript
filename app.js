423621f2970c78e6286b4d594aa9e9e5ea49614d
Sajit Maharjan <sajitmhr13@gmail.com>
2025-01-22 15:32:14 +0545

    feat: use of constructor

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 "use strict";
-let a = 23;
-//use of Enumeration
-var UserROles;
-(function (UserROles) {
-    UserROles["admin"] = "admin";
-    UserROles["guest"] = "guest";
-})(UserROles || (UserROles = {}));
-UserROles.admin;
-function PersonDetails(data) {
-    console.log(data);
+//  let a= 23;
+//  //use of Enumeration
+//  enum UserROles{
+//     admin = "admin",
+//     guest ="guest"
+//  }
+//  UserROles.admin;
+// //use of interfaces
+// interface Person {
+//    name: String,
+//    age: Number,
+//    height: Number,
+//    gender?: String //?denote as optional
+// }
+// function PersonDetails(data: Person) {
+//    console.log(data);
+// }
+// PersonDetails({name:"sajit", age:21 , height:5.6});
+// //type aliases
+// type arg= string | null ;
+//  function abcd(obj :arg)
+//  {
+//  }
+//  abcd("harsh")
+//  ///
+//use of constructor
+class person {
+    constructor(name, age = 0) {
+        this.name = name;
+        this.age = age;
+    }
 }
-PersonDetails({ name: "sajit", age: 21, height: 5.6 });
-function abcd(obj) {
-}
-abcd("harsh");
+let p = new person("sajit", 21);
+console.log('helllo boys');
+console.log(p.name);
+
--- app.ts
+++ app.ts
@@ -1,40 +1,56 @@
- let a= 23;
+//  let a= 23;
 
 
- //use of Enumeration
- enum UserROles{
-    admin = "admin",
-    guest ="guest"
- }
+//  //use of Enumeration
+//  enum UserROles{
+//     admin = "admin",
+//     guest ="guest"
+//  }
 
- UserROles.admin;
+//  UserROles.admin;
 
  
 
 
-//use of interfaces
+// //use of interfaces
 
-interface Person {
-   name: String,
-   age: Number,
-   height: Number,
-   gender?: String //?denote as optional
-}
+// interface Person {
+//    name: String,
+//    age: Number,
+//    height: Number,
+//    gender?: String //?denote as optional
+// }
 
-function PersonDetails(data: Person) {
-   console.log(data);
-}
+// function PersonDetails(data: Person) {
+//    console.log(data);
+// }
 
-PersonDetails({name:"sajit", age:21 , height:5.6});
+// PersonDetails({name:"sajit", age:21 , height:5.6});
 
 
 
-//type aliases
+// //type aliases
 
-type arg= string | null ;
- function abcd(obj :arg)
+// type arg= string | null ;
+//  function abcd(obj :arg)
+//  {
+
+//  }
+
+//  abcd("harsh")
+//  ///
+
+
+ //use of constructor
+ //in ts we exclude the  public name part outside the constructor and also exclude the this.name=name part
+
+ class person{
+   constructor(public name: string, public age: number =0)
+//for deafult value 
  {
 
+   }
  }
 
- abcd("harsh")
+ let p= new person("sajit", 21)
+ console.log(p.name);
